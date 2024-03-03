2ddfb277d0c71dfd190ec90db9af453c489b1c0a
Sk Alamin <alamin695sk@gmail.com>
2024-03-03 12:28:17 +0530

    book ticket

--- src/main/java/com/example/bms/BmsApplication.java
+++ src/main/java/com/example/bms/BmsApplication.java
@@ -1,13 +1,18 @@
 package com.example.bms;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BmsApplication {
+public class BmsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BmsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// we can call any controller here to run those particular api
+	}
 }
